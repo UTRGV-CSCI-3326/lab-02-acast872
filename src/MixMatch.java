de86346415a90ff208746bd3ec0b17832c27d050
acast872 <140276833+acast872@users.noreply.github.com>
2025-02-03 04:29:44 +0000

    Lab2changes

---
+++ src/MixMatch.java
@@ -0,0 +1,31 @@
+public class MixMatch{
+
+    public static void main(String[] args){
+    
+    String varWord = "Hello";
+            System.out.println(varWord);
+
+    char varChar= 'A';
+            System.out.println(varChar);
+
+    short varShor = 32766;
+            System.out.println(varShor);
+
+    int varInt = 2147483646;
+            System.out.println(varInt);
+
+    long varLong = 9223372036854775800L;
+            System.out.println(varLong);
+
+    double varDoub = 1.1245348764342;
+            System.out.println(varDoub);
+
+    float varFloat = 1.23456f;
+            System.out.println(varFloat);
+
+    boolean varBool = false;
+            System.out.println(varBool);
+
+
+    }
+}
